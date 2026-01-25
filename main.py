160204529a88f08f9f41cba4323140e0c4b59a57
RevelatorM <152555320+RevelatorM@users.noreply.github.com>
2026-01-25 17:04:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ from tkinter import * #importing everything from tkinter
 #widgets - GUI elements
 #windows - container
 
+#===================================
+def submit():
+
+#===================================
+
 window = Tk() #creating a window
+
+#===================================
+submit = Button(window, text = "submit",command = submit) #creating a button
+entry = Entry() #declaring a textbox
+entry.config(font=("Courier New",20)) #changing font and text size
+entry.insert(0,"write here!") #creating basic text
+entry.pack() #placing a textbox
 window.geometry("500x500") #creating a size of a window
 window.title("App") #creating a name of Application
 window.mainloop() #event listener and windowloop
+
